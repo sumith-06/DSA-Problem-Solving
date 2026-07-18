32f090dc74428c1cd3a7b4ab0a50a878a77266db
sumith-06 <agirisumith06@gmail.com>
2026-07-19 01:47:46 +0530

    Add problem - 268.Missing number

---
+++ 268_missing_number.py
@@ -0,0 +1,17 @@
+# Leetcode problem: 268. Missing Number
+
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        expected_sum = n*(n+1)//2
+        actual_sum = sum(nums)
+        return expected_sum - actual_sum
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)  
+
+# Example:
+# Input: nums = [9,6,4,2,3,5,7,0,1]
+# Output: 8
